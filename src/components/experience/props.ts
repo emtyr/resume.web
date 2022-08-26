b149b4f43dddf67266848ebc639dff4e468fce96
Flavius Emanuel <flavius.emanuel@gmail.com>
2022-08-26 10:33:12 +0300

    propsave

--- src/components/experience/props.ts
+++ src/components/experience/props.ts
@@ -1,14 +1,13 @@
 export interface XPCardProps {
-  text: string;
-  side: 'left' | 'right';
   startDate: string;
   endDate: string;
   company: string;
   position: string;
+  text: string;
+  side: 'left' | 'right';
 }
 
 export interface BulaProps {
   startDate: string;
   endDate: string;
 }
-
